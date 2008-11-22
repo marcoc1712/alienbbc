f483d37494db232b0b2350e96b7378d7ca539837
triode1@btinternet.com <triode1@btinternet.com@d4e98daa-b8a3-11dd-bdb3-db5ec97ac920>
2008-11-22 19:41:08 +0000

    add feedback on mplayer status to settings page

--- trunk/Alien/WindowsDownloader.pm
+++ trunk/Alien/WindowsDownloader.pm
@@ -9,8 +9,6 @@ use Archive::Zip qw(:ERROR_CODES);
 use Slim::Utils::Log;
 use Slim::Utils::Prefs;
 
-my $log = logger('plugin.alienbbc');
-
 my $mplayer = 'mplayer.exe';
 my $zipUrl  = 'http://www.mplayerhq.hu/MPlayer/releases/win32/MPlayer-mingw32-1.0rc2.zip';
 my $md5Url  = 'http://www.mplayerhq.hu/MPlayer/releases/win32/MPlayer-mingw32-1.0rc2.zip.md5';
@@ -19,27 +17,28 @@ sub checkMplayer {
 	my $class  = shift;
 	my $plugin = shift;
 
-	my $bindir = catdir($plugin->_pluginDataFor('basedir'), 'Bin');
+	my $bindir = catdir($plugin->pluginDir, 'Bin');
 
 	if (-e catdir($bindir, $mplayer)) {
-		$log->info("$mplayer found in $bindir");
+		$plugin->mplayer('found');
 		return 1;
 	}
 
 	if (! -w $bindir) {
-		$log->warn("can't download mplayer as $bindir is not writable");
+		$plugin->mplayer('download_failed', "$bindir is not writable");
 		return 0;
 	}
-
-	$log->warn("$mplayer missing - attempting to download");
 
 	my $cache = preferences('server')->get('cachedir');
 
 	my $data = {
 		bindir => $bindir,
+		plugin => $plugin,
 		zip    => catdir($cache, 'mplayer.zip'),
 		remaining => 2,
 	};
+
+	$plugin->mplayer('downloading');
 
 	Slim::Networking::SimpleAsyncHTTP->new(\&_gotFile, \&_downloadError, { saveAs => $data->{'zip'}, data => $data } )->get($zipUrl);
 	Slim::Networking::SimpleAsyncHTTP->new(\&_gotMD5,  \&_downloadError, { data => $data } )->get($md5Url);
@@ -68,13 +67,15 @@ sub _gotMD5 {
 sub _downloadError {
 	my $http  = shift;
 	my $error = shift;
+	my $data = $http->params('data');
 
-	$log->warn("unable to download " . $http->url . " - " . $error);
+	$data->{'plugin'}->mplayer('download_failed', $http->url . " - " . $error);
 }
 
 sub _extract {
 	my $data = shift;
 	my $file = $data->{'zip'};
+	my $plugin = $data->{'plugin'};
 
 	my $md5 = Digest::MD5->new;
 
@@ -88,7 +89,7 @@ sub _extract {
 		
 	if ($data->{'md5'} ne $md5->hexdigest) {
 
-		$log->warn("downloaded file does not match m5d checksum");
+		$plugin->mplayer('download_failed', 'bad md5 checksum');
 
 	} else {
 
@@ -96,22 +97,26 @@ sub _extract {
 
 		if ($zip->read($file) != AZ_OK) {
 
-			$log->warn("error reading zip file $file");
+			$plugin->mplayer('download_failed', "error reading zip file $file");
 
 		} elsif (my @file = $zip->membersMatching($mplayer)) {
 
 			if ($zip->extractMember($file[0], catdir($data->{'bindir'}, $mplayer)) != AZ_OK) {
 			
-				$log->warn("error extracting $mplayer from $file");
+				$plugin->mplayer('download_failed', "error extracting $mplayer from $file");
+
+			} if (Slim::Utils::Misc::findbin('mplayer')) {
+
+				$plugin->mplayer('download_ok');
 
 			} else {
 
-				$log->info("$mplayer extracted to $data->{bindir}");
+				$plugin->mplayer('download_failed', 'extracted but not found by findbin');
 			}
 
 		} else {
 
-			$log->warn("error extracting - can't find $mplayer in $file");
+			$plugin->mplayer('download_failed', "can't find $mplayer in $file");
 		}
 	}
 	
